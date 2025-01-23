cdccce43ae3c6fdcc92640d33998cc06eeb7f8eb
zeratheuz <zeratheuz@gmail.com>
2025-01-23 16:38:47 -0300

    Write the logic to get the computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,13 @@
-console.log("Hello Zerath!")
+function getComputerChoice () {
+  randomNumber = Math.ceil(Math.random() * 3)
+  switch (randomNumber) {
+    case 1:
+      return "rock"
+    case 2:
+      return "paper"
+    case 3:
+      return "scissors"
+  }
+}
+
+console.log(getComputerChoice())
