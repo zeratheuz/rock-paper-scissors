d5d9d1289513a5fb3520bee04773581f7bb3a24a
zeratheuz <zeratheuz@gmail.com>
2025-01-23 18:49:19 -0300

    Write the logic to play a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
   let randomNumber = Math.ceil(Math.random() * 3)
   switch (randomNumber) {
     case 1:
@@ -10,10 +10,49 @@ function getComputerChoice () {
   }
 }
 
-function getHumanChoice () {
-  let humanChoice = prompt("Rock, Paper or Scissors?")
-  return humanChoice
+function getHumanChoice() {
+  let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
+  if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+    return humanChoice
+  } else {
+    humanChoice = getHumanChoice()
+    return humanChoice
+  }
 }
 
 let humanScore = 0
 let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase()
+  if (humanChoice === computerChoice) {
+    console.log(`
+      DRAW
+      SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    computerScore++
+    console.log(`
+      You Lose! Paper beats Rock!
+      SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    computerScore++
+    console.log(`
+      You Lose! Rock beats Scissors!
+      SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    humanScore++
+    console.log(`
+      You Win! Paper beats Rock!
+      SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    humanScore++
+    console.log(`
+      You Win! Rock beats Scissors!
+      SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+  }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
