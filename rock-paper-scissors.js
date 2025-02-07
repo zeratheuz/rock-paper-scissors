e1925fc0645bb1fce15afd3efa5d7f543584b8e4
zeratheuz <zeratheuz@gmail.com>
2025-02-07 11:35:01 -0300

    Display results and score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -16,19 +16,27 @@ function getHumanChoice(e) {
 
 let humanScore = 0
 let computerScore = 0
+const score = document.querySelector("#score")
+score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
+const container = document.querySelector("#container")
+const result = document.createElement("div")
+container.appendChild(result)
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    console.log("DRAW", `\nSCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+    score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
+    result.textContent = `DRAW! ${computerChoice} is equal ${humanChoice}!`
     } else if (
       humanChoice == "rock" && computerChoice == "paper" || 
       humanChoice == "paper" && computerChoice == "scissors" || 
       humanChoice == "scissors" && computerChoice == "rock") {
     computerScore++
-    console.log(`You Lose! ${computerChoice} beats ${humanChoice}! \nSCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+    result.textContent = `You Lose! ${computerChoice} beats ${humanChoice}!`
+    score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
   } else {
     humanScore++
-    console.log(`You Win! ${humanChoice} beats ${computerChoice}! \nSCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+    result.textContent = `You Win! ${humanChoice} beats ${computerChoice}!`
+    score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
   }
 }
 
