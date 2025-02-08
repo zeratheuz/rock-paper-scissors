6c0b6e33186921bed145d13c5d9ec09ef4b32c96
zeratheuz <zeratheuz@gmail.com>
2025-02-08 20:38:03 -0300

    Add images and CSS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -16,35 +16,33 @@ function getHumanChoice(e) {
 
 let humanScore = 0
 let computerScore = 0
-const score = document.querySelector("#score")
-score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
 const container = document.querySelector("#container")
-const result = document.createElement("div")
-container.appendChild(result)
+const result = document.querySelector("#result")
+const displayHumanScore = document.querySelector("#humanScore")
+const displayComputerScore = document.querySelector("#computerScore")
 
 function playRound(humanChoice, computerChoice) {
-  if (humanScore === 5 || computerScore  === 5){
+  if (humanScore === 5 || computerScore === 5){
     if (humanScore > computerScore) {
-      alert("YOU WIN!" + `\nSCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+      alert("YOU WIN!")
     } else {
-      alert("YOU LOSE!" + `\nSCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`)
+      alert("YOU LOSE!")
     } 
     location.reload()
   } else {
     if (humanChoice === computerChoice) {
-      score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
       result.textContent = `DRAW! ${computerChoice} is equal ${humanChoice}!`
       } else if (
         humanChoice == "rock" && computerChoice == "paper" || 
         humanChoice == "paper" && computerChoice == "scissors" || 
         humanChoice == "scissors" && computerChoice == "rock") {
-      computerScore++
-      result.textContent = `You Lose! ${computerChoice} beats ${humanChoice}!`
-      score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
-    } else {
-      humanScore++
-      result.textContent = `You Win! ${humanChoice} beats ${computerChoice}!`
-      score.textContent = `SCORE: HUMAN = ${humanScore} || COMPUTER = ${computerScore}`
+          computerScore++
+          displayComputerScore.textContent = computerScore
+          result.textContent = `You Lose! ${computerChoice} beats ${humanChoice}!`
+      } else {
+        humanScore++
+        displayHumanScore.textContent = humanScore
+        result.textContent = `You Win! ${humanChoice} beats ${computerChoice}!`
     }
   }
 }
